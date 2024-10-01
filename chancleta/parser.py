737fa08184529f6abce49e45c750dec4e79201d1
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-10-01 23:38:21 +0300

    added dest, is_flag, description

--- chancleta/parser.py
+++ chancleta/parser.py
@@ -19,7 +19,8 @@ class Chancleta:
     # fmt: on
 
     MISSING_TABLE_ERROR = "Missing mandatory 'meta' table"
-    MISSING_KEY_ERROR = "Missing key '{key}' in table '{table}'"
+    MISSING_KEY_IN_TABLE_ERROR = "Missing key '{key}' in table '{table}'"
+    MISSING_PARAM_NAME_ERROR = "Missing mandatory 'name' for parameter '{parameter}'"
     NOT_LIST_ERROR = "'{key}' is not a list"
 
     def __init__(self, cwd="./"):
@@ -75,46 +76,65 @@ class Chancleta:
                             parser.description = sv
 
                 if func_src is None:
-                    raise KeyError(self.MISSING_KEY_ERROR.format(key="src", table="meta"))
+                    raise KeyError(self.MISSING_KEY_IN_TABLE_ERROR.format(key="src", table="meta"))
                 continue
 
             func_name = v.get("function", None)
             if func_name is None:
-                raise KeyError(self.MISSING_KEY_ERROR.format(key="function", table=k))
+                raise KeyError(self.MISSING_KEY_IN_TABLE_ERROR.format(key="function", table=k))
             func = getattr(import_module(func_src), func_name)  # noqa
 
-            # TODO: "help" etc should be optional
-            subparser = subparsers.add_parser(k, help=v["help"])
+            subparser = subparsers.add_parser(k, help=v.get("help", None))
             for sk, sv in v.items():
-                # TODO: simplify -> if sk in ("argument", "arguments")
-                if sk == "arguments" or (self.config_type == "xml" and sk == "argument"):
+                if sk in ("argument", "arguments"):
                     if not isinstance(sv, list):
                         if self.config_type != "xml":
                             raise TypeError(self.NOT_LIST_ERROR.format(key="arguments", table=k))
-                        subparser.add_argument(sv["name"], help=sv["help"])
+                        if not (name := sv.get("name", None)):
+                            raise KeyError(self.MISSING_PARAM_NAME_ERROR.format(parameter=sk))
+                        subparser.add_argument(
+                            name,
+                            help=sv.get("help", None),
+                            action="store_true" if sv.get("is_flag", None) == "True" else None,
+                        )
                     else:
                         for arg in sv:
-                            subparser.add_argument(arg["name"], help=arg["help"])
-                elif sk == "options" or (self.config_type == "xml" and sk == "option"):
+                            if not (name := arg.get("name", None)):
+                                raise KeyError(self.MISSING_PARAM_NAME_ERROR.format(parameter=sk))
+                            subparser.add_argument(
+                                name,
+                                help=arg.get("help", None),
+                                action="store_true" if arg.get("is_flag", None) == "True" else None,
+                            )
+                elif sk in ("option", "options"):
                     if not isinstance(sv, list):
                         if self.config_type != "xml":
                             raise TypeError(self.NOT_LIST_ERROR.format(key="options", table=k))
-                        # TODO: split and incrementally concatenate strings
+                        if not (name := sv.get("name", None)):
+                            raise KeyError(self.MISSING_PARAM_NAME_ERROR.format(parameter=sk))
                         subparser.add_argument(
-                            f"--{sv['name']}",
-                            f"-{sv['short']}",
-                            default=sv["default"],
-                            help=sv["help"],
+                            f"--{name}",
+                            f"-{sv['short'] if sv.get('short', None) else name[0]}",
+                            default=sv.get("default", None),
+                            action="store_true" if sv.get("is_flag", None) == "True" else None,
+                            dest=sv.get("dest", None),  # TODO: dest works only for options
+                            help=sv.get("help", None),
                         )
                     else:
                         for opt in sv:
+                            if not (name := opt.get("name", None)):
+                                raise KeyError(self.MISSING_PARAM_NAME_ERROR.format(parameter=sk))
                             subparser.add_argument(
-                                f"--{opt['name']}",
-                                f"-{opt['short']}",
-                                default=opt["default"],
-                                help=opt["help"],
+                                f"--{name}",
+                                f"-{opt['short'] if opt.get('short', None) else name[0]}",
+                                default=opt.get("default", None),
+                                action="store_true" if opt.get("is_flag", None) == "True" else None,
+                                dest=opt.get("dest", None),
+                                help=opt.get("help", None),
                             )
-
+                # TODO: implement
+                elif sk == "description":
+                    subparser.description = sv
             subparser.set_defaults(func=func)
         return parser.parse_args()
 
--- chancleta/util/commands.py
+++ chancleta/util/commands.py
@@ -1,7 +1,8 @@
-def echo(text, other_text, delimiter):
-    print(f"{text}{delimiter}{other_text}")
+def echo(text, other, delimiter):
+    print(f"{text}{delimiter}{other}")
 
 
-def yes(text):
-    for _ in range(5):
+def yes(text, should_repeat):
+    times = 5 if should_repeat else 2
+    for _ in range(times):
         print(text)
