01b375f82ba134525e07ce751269dd06ba04b71a
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-10-03 00:21:39 +0300

    updated config files, readme and project description; renamed variables and refactored code

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 ![Python 3.x](https://img.shields.io/badge/python-3.11-blue?style=flat-square&logo=Python&logoColor=white)
 [![PyPI](https://img.shields.io/pypi/v/chancleta.svg)](https://pypi.org/project/chancleta/)
 
-<br>Python library for <b>creating command line interfaces</b> in a composable way via configuration files.
+<br>Python library for creating <b>command line interfaces</b> in a composable way via configuration files.
 <br> Supports <i>.toml</i>, <i>.json</i>, <i>.yaml</i>. and (if you'd like to be more adventurous) <i>.xml</i>.
--- chancleta/parser.py
+++ chancleta/parser.py
@@ -69,89 +69,89 @@ class Chancleta:
         if "meta" not in self.data:
             raise KeyError(self.MISSING_TABLE_ERROR)
 
-        for k, v in self.data.items():
-            if not isinstance(v, dict):
+        for table_name, table_data in self.data.items():
+            if not isinstance(table_data, dict):
                 raise TypeError("Top level tables are not supported")
 
-            if k == "meta":
-                self._read_meta_table(k, v)
+            if table_name == "meta":
+                self._read_meta_table(table_name, table_data)
                 continue
             # NB: if we don't pass 'help' as kwarg here and add it later as attribute it gets ignored for some reason?!
             subparser = self.subparsers.add_parser(
-                k, help=v.get("help", None), prog=self.parser.prog
-            )  # TODO: check if manually setting 'prog' is necessary
-            for sk, sv in v.items():
-                match sk:
+                table_name, help=table_data.get("help", None), prog=self.parser.prog
+            )
+            for entry_name, entry_data in table_data.items():
+                match entry_name:
                     case "argument" | "arguments":
-                        self._handle_subparser_args(k, sk, subparser, sv)
+                        self._handle_subparser_args(table_name, entry_name, entry_data, subparser)
                     case "option" | "options":
-                        self._handle_subparser_args(k, sk, subparser, sv, is_option=True)
+                        self._handle_subparser_args(table_name, entry_name, entry_data, subparser, is_option=True)
                     case "description":
-                        subparser.description = sv
+                        subparser.description = entry_data
 
-            if not (func_name := v.get("function", None)):
-                raise KeyError(self.MISSING_TABLE_KEY_ERROR.format(key="function", table=k))
+            if not (func_name := table_data.get("function", None)):
+                raise KeyError(self.MISSING_TABLE_KEY_ERROR.format(key="function", table=table_name))
             subparser.set_defaults(func=getattr(importlib.import_module(self.func_src), func_name))  # noqa
         self.func_args = self.parser.parse_args()
 
     # ### read 'meta' ###
-    def _read_meta_table(self, k, v):
-        self.func_src = v.get("src", None)
+    def _read_meta_table(self, table_name, table_data):
+        self.func_src = table_data.get("src", None)
         if not self.func_src:
-            raise KeyError(self.MISSING_TABLE_KEY_ERROR.format(key="src", table=k))
-        for sk, sv in v.items():
-            match sk:
+            raise KeyError(self.MISSING_TABLE_KEY_ERROR.format(key="src", table=table_name))
+        for entry_name, entry_data in table_data.items():
+            match entry_name:
                 case "prog":
-                    self.parser.prog = sv
+                    self.parser.prog = entry_data
                 case "description":
-                    self.parser.description = sv
+                    self.parser.description = entry_data
                 case "usage":
-                    self.parser.usage = sv
+                    self.parser.usage = entry_data
                 case "version":
-                    self.parser.add_argument("--version", action="version", version=sv)
+                    self.parser.add_argument("--version", action="version", version=entry_data)
 
     # ### handle sub-commands ###
-    def _handle_subparser_args(self, k, sk, subparser, sv, is_option=False):
-        if isinstance(sv, list):
-            for arg in sv:
-                self._add_subparser_arg(k, sk, arg, subparser, is_option)
+    def _handle_subparser_args(self, table_name, entry_name, entry_data, subparser, is_option=False):
+        if isinstance(entry_data, list):
+            for argument in entry_data:
+                self._add_subparser_arg(table_name, entry_name, argument, subparser, is_option)
         else:
-            self._add_subparser_arg(k, sk, sv, subparser, is_option)
+            self._add_subparser_arg(table_name, entry_name, entry_data, subparser, is_option)
 
-    def _add_subparser_arg(self, k, sk, sv, subparser, is_option):
+    def _add_subparser_arg(self, table_name, entry_name, entry_data, subparser, is_option):
         import builtins
 
-        if not (name := sv.get("name", None)):
-            raise KeyError(self.MISSING_PARAM_KEY_ERROR.format(key="name", parameter=sk, table=k))
+        if not (name := entry_data.get("name", None)):
+            raise KeyError(self.MISSING_PARAM_KEY_ERROR.format(key="name", parameter=entry_name, table=table_name))
         kwargs = {
-            "help": sv.get("help", None),
+            "help": entry_data.get("help", None),
         }
         if not is_option:
             args = (name,)
             kwargs["action"] = "store"
         else:
-            args = (f"--{name}", f"-{sv['short'] if sv.get('short', None) else name[0]}")
+            args = (f"--{name}", f"-{entry_data['short'] if entry_data.get('short', None) else name[0]}")
             kwargs.update(
                 {
-                    "default": sv.get("default", None),
-                    "dest": sv.get("dest", None),  # works only for options
-                    "action": self._flag_action(sv),
+                    "default": entry_data.get("default", None),
+                    "dest": entry_data.get("dest", None),
+                    "action": self._flag_action(entry_data),
                 }
             )
         # for arguments and options -> if not booleans
         if kwargs["action"] == "store":
             kwargs.update(
                 {
-                    "type": getattr(builtins, arg_type) if (arg_type := sv.get("type", None)) else None,
-                    "nargs": sv.get("nargs", None),
-                    "choices": sv.get("choices", None),
+                    "type": getattr(builtins, arg_type) if (arg_type := entry_data.get("type", None)) else None,
+                    "nargs": entry_data.get("nargs", None),
+                    "choices": entry_data.get("choices", None),
                 }
             )
         subparser.add_argument(*args, **kwargs)
 
     @staticmethod
-    def _flag_action(sv):
-        match sv.get("flag", None):
+    def _flag_action(entry_data):
+        match entry_data.get("flag", None):
             case "True":
                 return "store_true"
             case "False":
@@ -163,7 +163,7 @@ class Chancleta:
         import inspect
 
         func = self.func_args.func
-        params = {p: getattr(self.func_args, p) for p in inspect.signature(func).parameters}
+        params = {param: getattr(self.func_args, param) for param in inspect.signature(func).parameters}
         func(**params)
 
     def parse(self):
